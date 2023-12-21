76611cd192194ca55770429b5e55e29cdee93033
Vinay Sanjay Ambatkar <96228448+VinayAmbatkar@users.noreply.github.com>
2023-12-21 19:39:44 +0530

    script and style uploaded

---
+++ script.js
@@ -0,0 +1,5 @@
+function smileOnClick() {
+    const smileLine = document.querySelector(".smile-line");
+    smileLine.style.width = "100px"; // Adjust the width to your preference
+    smileLine.style.borderRadius = "50% 50% 0 0"; // Curved like a smile
+}
